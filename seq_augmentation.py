d00084deb8bf5d2ce58c99a0e1ab4e041c69d4c6
Nadezda Kirillova <nadezda.kirillova@icg.tugraz.at>
2024-03-03 22:15:37 +0100

    Initial commit

--- seq_augmentation.py
+++ seq_augmentation.py
@@ -23,29 +23,6 @@ if __name__ == '__main__':
 
     print(f"Processing: {clear_path.name}")
 
-    # from perlin_numpy import generate_fractal_noise_2d
-    # from matplotlib import pyplot as plt
-    # from DepthEstimation.monocular_depth import normalize
-    # import numpy as np
-    # import cv2
-    # np.random.seed(111)
-    # noise = generate_fractal_noise_2d((1024, 1280), (4, 4), 5)
-    # noise = cv2.resize(noise, (1920, 1080))
-    # noise = normalize(noise)
-    # noise = (noise * 255).astype(np.uint8)
-    # plt.figure()
-    # plt.imshow(noise, cmap='gray')
-    # plt.show()
-    # plt.close()
-    # image_list = sorted(list(clear_path.glob('*')))
-    # img = cv2.imread(str(image_list[0]))
-    # #img = cv2.cvtColor(cv2.imread(str(image_list[0])), cv2.COLOR_BGR2GRAY)
-    # noise = cv2.merge([noise, noise, noise])
-    # fog_img = cv2.addWeighted(img, 0.5, noise, 0.5, 0)
-    # cv2.imshow('Noise', noise)
-    # cv2.imshow('Foggy image', fog_img)
-    # cv2.waitKey(0)
-
     image_list = sorted(list(clear_path.glob('*')))
     depth_maps = monocular_depth.run(image_list=image_list,
                                      output_path=output_path,
